5bd56afc052068f29b77b996baa366d802150746
Nathal <nathaldawsondiaz@gmail.com>
2020-11-24 19:14:21 -0300

    Trabajando con Raycast

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class TouchInput : MonoBehaviour
 {
+    #region CLASS_VARIABLES
+    private Touch touch;    
+    private bool touchingScreen;
+    #endregion
+
+    private void Start() {
+        touchingScreen = false;
+    }
+
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
@@ -13,20 +22,62 @@ public class TouchInput : MonoBehaviour
         {
             Debug.Log("Se está tocando la pantalla del dispositivo");
 
-            Touch touch = Input.GetTouch(0);
+            touch = Input.GetTouch(0);
 
             switch(touch.phase)
             {
                 case TouchPhase.Began:
                     Debug.Log("Acaba de comenzar un toque en la pantalla");
+                    touchingScreen = true;
                     break;
                 case TouchPhase.Moved:
                     Debug.Log("Estoy moviendo el dedo por la pantalla");
+                    touchingScreen = true;
+                    break;
+                case TouchPhase.Stationary:
+                    Debug.Log("Estoy tocando la pantalla");
+                    touchingScreen = true;
                     break;
                 case TouchPhase.Ended:
                     Debug.Log("Ya no estoy tocando la pantalla");
+                    touchingScreen = false;
+                    break;
+                case TouchPhase.Canceled:
+                    Debug.Log("Se canceló el toque en la pantalla");
+                    touchingScreen = false;
                     break;
             }
         }
+    }
+
+    private void FixedUpdate() {
+        //Vector3 inputPosition = touch.position;
+        //if(Input.GetMouseButton(0))
+        //{
+
+        //if(touchingScreen)
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            float rayDistance = 100f;
+            Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.green);
+
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                //Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.red);
+                if(hit.collider != null)
+                {
+                    GameObject gameObjectOfHit = hit.collider.gameObject;
+                    gameObjectOfHit.transform.localScale += new Vector3(0.001f, 0.001f, 0.001f);
+                    print("Hit something!");
+                }
+            }
+        }else
+        {
+            print("Rayo nulo");
+        }
+
+        //}
     }
 }
