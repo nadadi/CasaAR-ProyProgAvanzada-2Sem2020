a8e295aca3e4e4a9148e6c789820e91707975e75
Nathal <nathaldawsondiaz@gmail.com>
2020-12-09 19:11:13 -0300

    Jugando con el proyecto

---
+++ Assets/Scripts/ChangingObjectsInAList.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ChangingObjectsInAList : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> references;
+    [SerializeField]
+    private GameObject cubePrefab;
+    [SerializeField]
+    private GameObject spherePrefab;
+
+    private void Awake() {
+        references = new List<GameObject>();
+        GameObject[] arrayOfGameObjects = GameObject.FindGameObjectsWithTag("Reference");
+        references = arrayOfGameObjects.ToList();
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            /*for(int index = 0; index < references.Count; index++)
+            {
+                references[index] = X;
+            }*/
+
+            foreach (GameObject item in references)
+            {
+                if(item.transform.childCount > 0)
+                {
+                    foreach (Transform child in item.transform)
+                    {
+                        Destroy(child.gameObject);
+                    }
+                }
+                GameObject go = Instantiate(cubePrefab, item.transform.position, Quaternion.identity);
+                go.transform.parent = item.transform;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            foreach (GameObject item in references)
+            {
+                if(item.transform.childCount > 0)
+                {
+                    foreach (Transform child in item.transform)
+                    {
+                        Destroy(child.gameObject);
+                    }
+                }
+                GameObject go = Instantiate(spherePrefab, item.transform.position, Quaternion.identity);
+                go.transform.parent = item.transform;
+            }
+        }
+
+        /*if(Input.GetKeyDown(KeyCode.F))
+        {
+            
+        }*/
+    }
+}
---
+++ Assets/Scripts/LocateObjectAbovePlane.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocateObjectAbovePlane : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject plane;
+    [SerializeField]
+    private GameObject prefabToInstantiate;
+
+    private void Update() {
+        //if(Input.GetMouseButtonDown(0))
+        //if(Input.GetButtonDown("Space"))
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject gameObjectToInstantiate = Instantiate(prefabToInstantiate, plane.transform.position, Quaternion.identity);
+            
+            BoxCollider boxCollider = gameObjectToInstantiate.GetComponent<BoxCollider>();
+            Vector3 positionToInstantiate = Vector3.zero;
+            if(boxCollider != null){
+                //Debug.Log(boxCollider.bounds.size.y);
+                positionToInstantiate = new Vector3(plane.transform.position.x, plane.transform.position.y + boxCollider.bounds.size.y/2, plane.transform.position.z);
+            }
+
+            gameObjectToInstantiate.transform.position = positionToInstantiate;
+        }
+    }
+}
